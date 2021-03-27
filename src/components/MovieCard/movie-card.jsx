96fa38da24c55f62c2dbcd124e06f4b91e6c195d
DelManzueta <dmanzueta@gmail.com>
2021-03-27 01:09:42 -0400

    added color palette to docs

--- src/components/MovieCard/movie-card.jsx
+++ src/components/MovieCard/movie-card.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
@@ -6,20 +5,12 @@ import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 
 import './movie-card.scss';
-=======
-import React from "react";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
-
-import "./movie-card.scss";
->>>>>>> 3.4
 
 export class MovieCard extends React.Component {
   render() {
     const { movie, onClick } = this.props;
 
     return (
-<<<<<<< HEAD
       <Container className="card-container">
         <Card className="movie-card">
           <Card.Img className="movie-poster" variant="top" src={movie.ImagePath} />
@@ -42,41 +33,3 @@ MovieCard.propTypes = {
   }).isRequired,
   onClick: PropTypes.func.isRequired
 };
-=======
-      <Card className="movie-card">
-        <Card.Header className="card-header">
-          {movie.Title} 
-        </Card.Header>
-
-        <Card.Img className="card-img" variant="top" src="https://dummyimage.com/720x250/CA3C25" />
-
-        <Card.Body>
-          <Card.Title>{movie.Description}</Card.Title>
-
-          <Button
-            className="sub-links"
-            onClick={() => onClick(movie)}
-            variant="link"
-          >
-            Genre
-          </Button>
-          <Button
-            className="sub-links"
-            onClick={() => onClick(movie)}
-            variant="link"
-          >
-            Director
-          </Button> 
-          <Button
-            className="sub-links"
-            onClick={() => onClick(movie)}
-            variant="link"
-          >
-            Back to Your Profile
-          </Button>
-        </Card.Body>
-      </Card>
-    );
-  }
-}
->>>>>>> 3.4
